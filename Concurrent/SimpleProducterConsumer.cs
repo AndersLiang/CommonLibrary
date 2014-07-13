5f9bb07e166f3c93abc54f0fd3785d95b0bbe7ec
Anders Liang <guogen.liang@outlook.com>
2014-07-13 22:33:56 +0800

    Simple Producter Consumer

---
+++ Concurrent/SimpleProducterConsumer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace Concurrent
+{
+    public class SimpleProducterConsumer
+    {
+        private ConcurrentQueue<int> mProducts = new ConcurrentQueue<int>();
+        private int mThreshold = 10;
+        private AutoResetEvent mProductAutoResetEvent = new AutoResetEvent(false);
+        private AutoResetEvent mConsumerAutoResetEvent = new AutoResetEvent(true);
+
+        public SimpleProducterConsumer()
+        {
+        }
+
+        public void Start()
+        {
+            var producter = Task.Factory.StartNew(() =>
+            {
+                foreach (var p in Products)
+                {
+                    Thread.Sleep(100);
+                    mProducts.Enqueue(p);
+                    mProductAutoResetEvent.Set();
+
+                    if (mProducts.Count > mThreshold)
+                        mConsumerAutoResetEvent.WaitOne();
+                }
+            });
+
+            var consumer1 = Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    int p = 0;
+                    if (mProducts.TryDequeue(out p))
+                    {
+                        Thread.Sleep(200);
+                        Console.WriteLine(string.Format("Consumer 1:{0}",p));
+                        mConsumerAutoResetEvent.Set();
+                    }
+                    else
+                        mProductAutoResetEvent.WaitOne();
+                }
+            });
+
+            var consumer2 = Task.Factory.StartNew(() =>
+           {
+               while (true)
+               {
+                   int p = 0;
+                   if (mProducts.TryDequeue(out p))
+                   {
+                       Thread.Sleep(300);
+                       Console.WriteLine(string.Format("Consumer 2:{0}", p));
+                       mConsumerAutoResetEvent.Set();
+                   }
+                   else
+                       mProductAutoResetEvent.WaitOne();
+               }
+           });
+
+            Task.WaitAll(consumer1, consumer2);
+        }
+
+        private IEnumerable<int> Products
+        {
+            get
+            {
+                int i = 0;
+                while (true)
+                    yield return i++;
+            }
+        }
+    }
+
+
+}
